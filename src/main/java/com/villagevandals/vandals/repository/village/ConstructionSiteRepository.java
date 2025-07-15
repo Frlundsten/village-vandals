0015ae83cbfba99aef27fa47526271ea2b175702
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-15 19:44:39 +0200

    Refactor to use new changelog data name

--- src/main/java/com/villagevandals/vandals/repository/village/ConstructionSiteRepository.java
+++ src/main/java/com/villagevandals/vandals/repository/village/ConstructionSiteRepository.java
@@ -1,12 +1,11 @@
 package com.villagevandals.vandals.repository.village;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ConstructionSiteRepository extends JpaRepository<ConstructionSite, Long> {
-  List<ConstructionSite> findByVillageResourceId(Long villageResourceId);
-  Optional<ConstructionSite> findByIdAndVillageResourceId(Long id, Long villageResourceId);
-  List<ConstructionSite> getConstructionSiteById(int id);
+ List<ConstructionSite> findByVillageId(Long villageId);
+ Optional<ConstructionSite> findByIdAndVillageId(Long id, Long villageId);
 }
