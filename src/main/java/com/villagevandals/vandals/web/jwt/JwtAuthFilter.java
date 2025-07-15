b93a50e266c2320531de3a75a56a1a693d3cb499
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-15 19:47:45 +0200

    Add filter to validate JWT

---
+++ src/main/java/com/villagevandals/vandals/web/jwt/JwtAuthFilter.java
@@ -0,0 +1,57 @@
+package com.villagevandals.vandals.web.jwt;
+
+import com.villagevandals.vandals.service.user.UserInfoService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+  public static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
+
+  private final UserInfoService userInfoService;
+  private final JwtService jwtService;
+
+  public JwtAuthFilter(UserInfoService userInfoService, JwtService jwtService) {
+    this.userInfoService = userInfoService;
+    this.jwtService = jwtService;
+  }
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+    String token = null;
+    String username = null;
+
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+      token = authHeader.substring(7);
+      username = jwtService.extractUsername(token);
+    }
+
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = userInfoService.loadUserByUsername(username);
+      logger.debug("Checking JWT for user: {}", username);
+      if (jwtService.validateToken(token, userDetails)) {
+        UsernamePasswordAuthenticationToken authToken =
+            new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
+}
