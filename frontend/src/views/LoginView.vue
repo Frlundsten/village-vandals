3501bb36fdebfbc37aef98b01454da850ec73194
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-15 20:01:51 +0200

    Add login view on the frontend

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,15 +1,26 @@
 <template>
-  <form @submit.prevent="login">
-    <input v-model="username" placeholder="Username" />
-    <input v-model="password" placeholder="Password" type="password" />
-    <button type="submit">Login</button>
-    <p v-if="error">{{ error }}</p>
-  </form>
+  <div class="flex items-center justify-center min-h-screen">
+    <div
+        class="flex items-center justify-center w-full min-h-screen bg-[url('../assets/jpeg/vv.png')] bg-cover bg-center bg-no-repeat">
+      <form
+          class="md:w-full max-w-xs min-w-[224px] mx-auto fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+          @submit.prevent="login">
+        <label class="label">Username</label>
+        <input class="input" v-model="username" placeholder="Username"/>
+        <label class="label">Password</label>
+        <input class="input" v-model="password" placeholder="Password" type="password"/>
+        <button class="btn btn-neutral mt-4" type="submit">Login</button>
+        <router-link to="/register" class="btn btn-link mt-4">Register</router-link>
+        <p v-if="error">{{ error }}</p>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import {ref} from 'vue'
+import {useRouter} from 'vue-router'
+import {useSessionStore} from '../stores/pinia.js'
 
 const username = ref('')
 const password = ref('')
@@ -17,25 +28,28 @@ const error = ref(null)
 const router = useRouter()
 
 async function login() {
-  error.value = null
+  error.value = null;
   try {
-    const params = new URLSearchParams();
-    params.append('username', username.value);
-    params.append('password', password.value);
-
-    const response = await fetch('http://localhost:8080/login', {
+    const response = await fetch('http://localhost:8080/user/auth/generateToken', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
       },
       credentials: 'include',
-      body: params,
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
     });
 
     if (!response.ok) {
-      throw new Error('Login failed')
+      throw new Error('Login failed');
     }
 
+    const token = await response.text();
+
+    const session = useSessionStore()
+    session.setToken(token);
     await router.push('/village')
   } catch (e) {
     error.value = 'Login failed, check credentials'
