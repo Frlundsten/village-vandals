04756f603bc6ca733c65922869bbe6ef3b88c5cc
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-15 19:41:51 +0200

    Use alt text for image placement

--- frontend/src/components/Avatar.vue
+++ frontend/src/components/Avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="avatar">
     <div class="w-24 rounded  shadow-[0_0_1px_2px_rgba(0,0,0,0.9)]">
-      <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
+      <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp"  alt="Avatar"/>
     </div>
   </div>
 </template>
