e1f5eb090269eb50ea1323851abb48cbe95f7110
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-15 19:46:50 +0200

    Add child routes to Home component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,54 +1,65 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 import World from "@/views/WorldView.vue";
 import Village from "@/components/Village.vue";
 import LoginView from "@/views/LoginView.vue";
 import {useSessionStore} from "@/stores/pinia.js";
+import RegisterView from "@/views/RegisterView.vue";
+import VillageNew from "@/components/villageNew.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/map',
-      name: 'world',
-      component: World,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/village',
-      name: 'village',
-      component: Village,
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+            meta: {requiresAuth: true},
+            children: [
+                {
+                    path: '',
+                    redirect: '/village'
+                },
+                {
+                    path: 'map',
+                    name: 'world',
+                    component: World,
+                    meta: {requiresAuth: true}
+                },
+                {
+                    path: 'village',
+                    name: 'village',
+                    component: VillageNew,
+                    meta: {requiresAuth: true}
+                },
+            ]
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import('../views/AboutView.vue'),
+        },
+    ],
 })
 
 router.beforeEach(async (to, from, next) => {
-  const session = useSessionStore()
+    const session = useSessionStore()
 
-  await session.checkSession()
+    if (to.meta.requiresAuth && !session.isAuthenticated) {
+        return next('/login')
+    }
 
-  if (to.meta.requiresAuth && !session.isAuthenticated) {
-    return next('/login')
-  }
-
-  next()
+    return next()
 })
 
 
