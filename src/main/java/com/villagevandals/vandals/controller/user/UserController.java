4f46efa93b2af716db1021d8d69659d6104a95a4
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-15 20:06:06 +0200

    UserController validates user

--- src/main/java/com/villagevandals/vandals/controller/user/UserController.java
+++ src/main/java/com/villagevandals/vandals/controller/user/UserController.java
@@ -1,24 +1,77 @@
 package com.villagevandals.vandals.controller.user;
 
+import com.villagevandals.vandals.web.AuthRequest;
+import com.villagevandals.vandals.web.jwt.JwtService;
 import com.villagevandals.vandals.service.user.UserService;
+import com.villagevandals.vandals.web.UserInfo;
 import java.security.Principal;
+import java.util.UUID;
+
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+  private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
-  UserService userService;
+  private final UserService userService;
 
-  UserController(UserService userService) {
+  private final JwtService jwtService;
+
+  private final AuthenticationManager authenticationManager;
+
+  UserController(
+      UserService userService, JwtService jwtService, AuthenticationManager authenticationManager) {
     this.userService = userService;
+    this.jwtService = jwtService;
+    this.authenticationManager = authenticationManager;
   }
 
   @GetMapping
   public UserDTO currentUserName(Principal principal) {
+    LOG.debug("Fetching user info...");
     String username = principal.getName();
     return userService.getUserInfo(username);
+  }
+
+    @GetMapping("/validate")
+    public void validateAuthentication() {
+    }
+
+  @PostMapping("/addNewUser")
+  public ResponseEntity<String> addNewUser(@Valid @RequestBody UserRegistrationDTO dto) {
+    UserInfo userInfo =
+        new UserInfo(UUID.randomUUID(), dto.username(), dto.email(), dto.password(), "ROLE_USER");
+    try {
+      userService.newUser(userInfo);
+      return ResponseEntity.ok("User created successfully");
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @PostMapping("/auth/generateToken")
+  public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+    Authentication authentication =
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                authRequest.username(), authRequest.password()));
+    if (authentication.isAuthenticated()) {
+      return jwtService.generateToken(authRequest.username());
+    } else {
+      throw new UsernameNotFoundException("Invalid user request!");
+    }
   }
 }
