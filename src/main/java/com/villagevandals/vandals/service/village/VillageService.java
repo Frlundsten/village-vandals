57e5e09f8b287b22507fadbafc7468595e13bf08
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-17 13:22:10 +0200

    Get starter village by user object

--- src/main/java/com/villagevandals/vandals/service/village/VillageService.java
+++ src/main/java/com/villagevandals/vandals/service/village/VillageService.java
@@ -2,9 +2,7 @@ package com.villagevandals.vandals.service.village;
 
 import com.villagevandals.vandals.model.domain.User;
 import com.villagevandals.vandals.model.domain.Village;
-import com.villagevandals.vandals.repository.user.UserResource;
 import com.villagevandals.vandals.repository.village.VillageRepository;
-import com.villagevandals.vandals.repository.village.VillageResource;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -22,7 +20,7 @@ public class VillageService {
     return Village.builder().startingVillage(user).build();
   }
 
-  public Optional<VillageResource> getStarterVillage(UserResource ur) {
-    return villageRepository.findByOwner(ur);
+  public Optional<Village> getStarterVillage(User owner) {
+    return villageRepository.findByOwner(owner);
   }
 }
