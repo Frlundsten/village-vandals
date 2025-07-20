4c8d2514571e7e4df500cbbc54fba13036a0fe9c
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-07-20 21:49:20 +0200

    Make code more readable

--- src/main/java/com/villagevandals/vandals/service/village/VillageService.java
+++ src/main/java/com/villagevandals/vandals/service/village/VillageService.java
@@ -1,11 +1,12 @@
 package com.villagevandals.vandals.service.village;
 
+import static com.villagevandals.vandals.model.domain.Village.initStarterVillage;
+
 import com.villagevandals.vandals.model.domain.User;
 import com.villagevandals.vandals.model.domain.Village;
 import com.villagevandals.vandals.repository.village.VillageRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.Optional;
+import org.springframework.stereotype.Service;
 
 @Service
 public class VillageService {
@@ -17,7 +18,7 @@ public class VillageService {
   }
 
   public Village starterVillage(User user) {
-    return Village.builder().startingVillage(user).build();
+    return initStarterVillage(user);
   }
 
   public Optional<Village> getStarterVillage(User owner) {
