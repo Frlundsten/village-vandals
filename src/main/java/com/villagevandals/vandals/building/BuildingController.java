23ccb4a4e94b93dc6b4762fa6111fc95b5892e6b
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-09-16 21:59:17 +0200

    Add logging and endpoints for buildings

--- src/main/java/com/villagevandals/vandals/building/BuildingController.java
+++ src/main/java/com/villagevandals/vandals/building/BuildingController.java
@@ -1,11 +1,11 @@
 package com.villagevandals.vandals.building;
 
 import com.villagevandals.vandals.building.buildings.Building;
-
+import com.villagevandals.vandals.building.dto.ConstructionRequestDTO;
 import java.security.Principal;
 import java.util.List;
-
-import com.villagevandals.vandals.building.dto.ConstructionRequestDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/building")
 public class BuildingController {
 
+  private static final Logger LOG = LoggerFactory.getLogger(BuildingController.class);
+
   BuildingService buildingService;
 
   public BuildingController(BuildingService buildingService) {
@@ -25,18 +27,31 @@ public class BuildingController {
   }
 
   @GetMapping
-  public List<Building> getAllBuildings(@RequestParam Long villageId, Principal principal) {
+  public List<Building> getExistingBuildings(@RequestParam Long villageId, Principal principal) {
     String username = principal.getName();
     return buildingService.getAllBuildingsByVillageId(villageId, username);
   }
 
   @PostMapping
   public ResponseEntity<?> createBuilding(@RequestBody ConstructionRequestDTO dto) {
+    LOG.debug("Got a request to construct: {}", dto);
     try {
       buildingService.constructBuilding(dto);
       return ResponseEntity.ok().build();
     } catch (Exception e) {
+        LOG.debug(e.getMessage());
       return ResponseEntity.badRequest().body("Unable to construct building");
+    }
+  }
+
+  @GetMapping("/available")
+  public List<Building> getAvailableBuildings(@RequestParam Long villageId, Principal principal) {
+    try {
+      List<Building> result = buildingService.getAvailableBuildings(villageId, principal.getName());
+      LOG.info("Available buildings: {}", result);
+      return result;
+    } catch (Exception e) {
+      return List.of();
     }
   }
 }
