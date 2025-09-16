8719f1455c550b6cbbdf2386aa2dbc43a44cfd4e
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-09-16 22:02:28 +0200

    Refactor constructionsite db data

--- src/main/java/com/villagevandals/vandals/building/dto/ConstructionRequestDTO.java
+++ src/main/java/com/villagevandals/vandals/building/dto/ConstructionRequestDTO.java
@@ -1,3 +1,6 @@
 package com.villagevandals.vandals.building.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ConstructionRequestDTO(String type, long constructionSiteId, long villageId) {}
--- src/main/java/com/villagevandals/vandals/constructionsite/ConstructionSiteRepository.java
+++ src/main/java/com/villagevandals/vandals/constructionsite/ConstructionSiteRepository.java
@@ -4,8 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ConstructionSiteRepository extends JpaRepository<ConstructionSite, Long> {
  List<ConstructionSite> findByVillageId(Long villageId);
+
+    @Query("SELECT cs FROM ConstructionSite cs " +
+            "WHERE cs.id = :id AND cs.village.id = :villageId")
  Optional<ConstructionSite> findByIdAndVillageId(Long id, Long villageId);
 }
