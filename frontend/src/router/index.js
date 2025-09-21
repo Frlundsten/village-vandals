12ed82a66e6e7b6ef3edd7ff97009469919472d6
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-09-21 21:11:02 +0200

    fix

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,73 +1,71 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import World from "@/views/WorldView.vue";
-import LoginView from "@/views/LoginView.vue";
-import {useSessionStore} from "@/stores/pinia.js";
-import RegisterView from "@/views/RegisterView.vue";
-import VillageNew from "@/components/villageNew.vue";
-import villageNew from "@/components/villageNew.vue";
+import World from '@/views/WorldView.vue'
+import LoginView from '@/views/LoginView.vue'
+import { useSessionStore } from '@/stores/pinia.js'
+import RegisterView from '@/views/RegisterView.vue'
+import VillageNew from '@/components/VillageNew.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta: { requiresAuth: true },
+      children: [
         {
-            path: '/',
-            name: 'home',
-            component: Home,
-            meta: {requiresAuth: true},
-            children: [
-                {
-                    path: '',
-                    redirect: '/village',
-                    props: true,
-                },
-                {
-                    path: 'village/:villageId',
-                    name: 'Village',
-                    component: villageNew,
-                    props: route => ({ villageId: Number(route.params.villageId) }),
-                },
-                {
-                    path: 'map',
-                    name: 'world',
-                    component: World,
-                    meta: {requiresAuth: true}
-                },
-                {
-                    path: 'village',
-                    name: 'village',
-                    component: VillageNew,
-                    meta: {requiresAuth: true}
-                },
-            ]
+          name: '-',
+          path: '',
+          redirect: '/village',
         },
         {
-            path: '/login',
-            name: 'login',
-            component: LoginView,
+          path: 'village/:villageId',
+          name: 'Village',
+          component: VillageNew,
+          // props: (route) => ({ villageId: Number(route.params.villageId) }),
         },
         {
-            path: '/register',
-            name: 'register',
-            component: RegisterView,
+          path: 'map',
+          name: 'world',
+          component: World,
+          meta: { requiresAuth: true },
         },
         {
-            path: '/about',
-            name: 'about',
-            component: () => import('../views/AboutView.vue'),
+          path: 'village',
+          name: 'village',
+          component: VillageNew,
+          meta: { requiresAuth: true },
         },
-    ],
+      ],
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
 })
 
 router.beforeEach(async (to, from, next) => {
-    const session = useSessionStore()
+  const session = useSessionStore()
 
-    if (to.meta.requiresAuth && !session.isAuthenticated) {
-        return next('/login')
-    }
+  if (to.meta.requiresAuth && !session.isAuthenticated) {
+    return next('/login')
+  }
 
-    return next()
+  return next()
 })
-
 
 export default router
