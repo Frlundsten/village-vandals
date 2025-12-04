b2e922ff4b2e815fccf7acbe08e7b5383a0b136d
Fredrik Lundsten <f.lundsten@hotmail.se>
2025-12-04 20:42:59 +0100

    fix: Validate starter village position based on existing

--- src/main/java/com/villagevandals/vandals/village/VillageService.java
+++ src/main/java/com/villagevandals/vandals/village/VillageService.java
@@ -2,22 +2,32 @@ package com.villagevandals.vandals.village;
 
 import static com.villagevandals.vandals.village.Village.initStarterVillage;
 
+import com.villagevandals.vandals.app.Tile;
+import com.villagevandals.vandals.app.TileRepository;
 import com.villagevandals.vandals.user.User;
-
 import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class VillageService {
-
+  TileRepository tileRepository;
   VillageRepository villageRepository;
 
-  public VillageService(VillageRepository villageRepository) {
+  public VillageService(VillageRepository villageRepository, TileRepository tileRepository) {
     this.villageRepository = villageRepository;
+    this.tileRepository = tileRepository;
   }
 
+  @Transactional
   public Village starterVillage(User user) {
-    return initStarterVillage(user);
+    Tile tile =
+        tileRepository
+            .findRandomFreeTile()
+            .orElseThrow(() -> new RuntimeException("No free tiles available"));
+    tileRepository.markOccupied(tile.getId());
+
+      return initStarterVillage(user, tile);
   }
 
   public Optional<Village> getStarterVillage(User owner) {
